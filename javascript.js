3e3d8a646e59fef6792716ea9719d55a7363b401
Mikkel Madsen <mikkelmadsen@mikkels-air.home>
2018-06-09 22:34:37 +0200

    nav wheel virker,

--- javascript.js
+++ javascript.js
@@ -22,8 +22,21 @@ function setScene() {
     $("#navTheme04").on("click", rotate);
     document.getElementById("navTheme04").pos = 0;
 
-    $("#sectionTheme01, #sectionTheme02, #sectionTheme03, #sectionTheme04").addClass("hide");
+    $("#sectionTheme01, #sectionTheme02, #sectionTheme03, #sectionTheme04, #sectionContentTheme03Left, #sectionContentTheme03Right, #sectionContentTheme04Left, #sectionContentTheme04Right").addClass("hide");
     $("#pathcontainer").addClass("hide");
+
+    $("#theme03Left").on("click", function () {
+        showSectionContent("Theme03", "left")
+    });
+    $("#theme03Right").on("click", function () {
+        showSectionContent("Theme03", "right")
+    });
+    $("#theme04Left").on("click", function () {
+        showSectionContent("Theme04", "left")
+    });
+    $("#theme04Right").on("click", function () {
+        showSectionContent("Theme04", "right")
+    });
 }
 
 function rotate(event) {
@@ -51,5 +64,18 @@ function showSection(event) {
     }
     $("#theme_one").removeClass("hide");
     $("#pathcontainer").removeClass("hide");
-    window.location.href = '#theme_one';
+}
+
+function showSectionContent(theme, side) {
+    const a = "#sectionContent" + theme + "Right";
+    console.log(theme, side);
+    console.log(a);
+    console.log($(a));
+    if (side === "left") {
+        $("#sectionContent" + theme + "Left").removeClass("hide");
+        $("#sectionContent" + theme + "Right").addClass("hide");
+    } else {
+        $("#sectionContent" + theme + "Left").addClass("hide");
+        $("#sectionContent" + theme + "Right").removeClass("hide");
+    }
 }
